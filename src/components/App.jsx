2f12dcab7bcf042831d4a1f7f2fe247c039549e6
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-04-20 00:01:30 +0300

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Title, ButtonList, Button, StatList } from './App.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  values = Object.values(this.state);
+  labels = Object.keys(this.state);
+
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+    const total = values.reduce((acc, value) => acc + value, 0);
+
+    if (!total) {
+      return false;
+    }
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const percentage = this.state.good / this.countTotalFeedback();
+
+    if (!percentage) {
+      return false;
+    }
+    return Math.round(percentage * 100);
+  };
+
+  handleClick = event => {
+    for (const key in this.state) {
+      if (key === event.target.textContent) {
+        this.setState({ [event.target.textContent]: this.state[key] + 1 });
+      }
+    }
+  };
+
+  render() {
+    const values = Object.values(this.state);
+
+    return (
+      <>
+        <Title>Please leave feedback</Title>
+        <ButtonList>
+          {this.labels.map(btn => {
+            return (
+              <li key={btn}>
+                <Button type="button" onClick={this.handleClick}>
+                  {btn}
+                </Button>
+              </li>
+            );
+          })}
+        </ButtonList>
+
+        <Title>Statistics</Title>
+        {this.countTotalFeedback() ? (
+          <ul>
+            {this.labels.map((label, i) => {
+              return (
+                <StatList key={label}>
+                  {label}: {values[i]}
+                </StatList>
+              );
+            })}
+            <StatList>Total: {this.countTotalFeedback()}</StatList>
+            {this.countPositiveFeedbackPercentage() && (
+              <StatList>
+                Positive feedback: {this.countPositiveFeedbackPercentage()}%
+              </StatList>
+            )}
+          </ul>
+        ) : (
+          <p>No feedback given</p>
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  font-weight: 700;
+  font-size: 24px;
+  margin: 24px 0;
+`;
+
+export const ButtonList = styled.ul`
+  display: inline-flex;
+`;
+
+export const Button = styled.button`
+  padding: 1px 5px;
+  text-transform: capitalize;
+  font-size: 12px;
+  font-weight: 600;
+  margin: 3px;
+  border: 1px solid #6e6e6e6c;
+  border-radius: 5px;
+`;
+
+export const StatList = styled.li`
+  text-transform: capitalize;
+  margin: 3px;
+`;
